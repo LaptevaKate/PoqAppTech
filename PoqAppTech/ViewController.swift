48cac53bb841b47a4a4a701ca63dff30ba34191b
Katsiaryna Laptseva <k.laptseva@andersenlab.com>
2024-07-10 15:40:35 +0300

    Initial Commit

---
+++ PoqAppTech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PoqAppTech
+//
+//  Created by Екатерина Лаптева on 10.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
